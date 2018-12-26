34f7b45d3fae7188afff725119f2386a8cd085ad
Shahaed Hasan <shahaed77@gmail.com>
2018-12-25 19:24:49 -0600

    Updated form data

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 let names = JSON.parse(fs.readFileSync("names.json")).names;
-let spamURL = 'https://www.mobileiman.com/imgs/sch/uiowa/uiowa.php';
+let spamURL = 'https://konfgersj.com/sch/uiowa/HawkID.htm';
 const axios = require('axios');
 let randomName = names[randomNumber(names.length)] + names[randomNumber(names.length)] + randomNumber(10);
 
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10000; i++) {
     axios.post(spamURL, {
         username: randomName,
-        password: randomPassword(randomNumber(15))
+        password: randomPassword(randomNumber(15)),
+        service: "http://myui.uiowa.edu/my-ui/home.page",
+        uip_nonPassportRequestParameters: "",
+        uip_action: "Log In"
     })
     .then((res) => {
-        console.log(`statusCode: ${res.statusCode}`)
+        console.log(`statusCode: ${res.status}`)
       })
     .catch((error) => {
         console.error(error)
